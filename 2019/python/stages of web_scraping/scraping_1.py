24a66890b7a5b76676ef11716446d047ff20f0a1
Daniel Röwenstrunk <roewenstrunk@uni-paderborn.de>
2019-10-29 13:01:56 +0100

    old-stuff

--- 2019/python/stages of web_scraping/scraping_1.py
+++
@@ -1,6 +0,0 @@
-# get all the libraries that we need.
-from bs4 import BeautifulSoup
-from urllib import request
-
-# store the base_url
-url = "https://github.com/humanitiesprogramming/scraping-corpus"
